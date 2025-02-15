501e54f24a15f9db5f361b55503a940198cfd49e
Ishaan Sen <ishaan.sen100@gmail.com>
2025-02-15 13:16:34 -0500

    small changes

--- src/subsystems/manipulator_controller.py
+++ src/subsystems/manipulator_controller.py
@@ -7,11 +7,12 @@ class ManipulatorController(Subsystem):
 
         self.controller = GenericHID(controller_id)
         self.button_states = [None for _ in range(32)]
-        self.joystick_states = [None for _ in range(32)]
-        self.branch_selection = None
+        self.joystick_states = [None for _ in range(6)]
+        self.tree_selection = None
+        self.height_selection = None
 
     def get_button_states(self):
-        self.button_states = [self.controller.getRawButton(i + 1) for i in range(32)] # one-indexed for some reason
+        self.button_states = [self.controller.getRawButtonPressed(i + 1) for i in range(32)] # one-indexed for some reason
 
     def get_joystick_states(self):
         self.joystick_states = [self.controller.getRawAxis(i) for i in range(6)]
@@ -19,10 +20,15 @@ class ManipulatorController(Subsystem):
     def periodic(self):
         self.get_button_states()
         self.get_joystick_states()
+        self.get_height_selection()
 
-    def get_branch_selection(self):
+    def get_tree_selection(self):
         def axis_to_selection(value):
             return round(6 * (value + 1)) % 12
 
-        self.branch_selection = axis_to_selection(self.joystick_states[0])
-        
+        self.tree_selection = axis_to_selection(self.joystick_states[0])
+
+    def get_height_selection(self):
+        for i in range(4):
+            if self.button_states[i]:
+                self.button_selection = i + 1 # the manual's naming convention starts at L1, so we'll stick with that
