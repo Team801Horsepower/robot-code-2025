b72652a0049a98575f9f11eb2d0ec7ef147e1854
TheNerdyFunnyComedian <hudsonjimenez@gmail.com>
2025-02-19 18:54:00 -0500

    Things make sense now, no presets yet.

--- src/subsystems/end_effector.py
+++ src/subsystems/end_effector.py
@@ -8,24 +8,24 @@ class EndEffector(Subsystem):
     def __init__(self, scheduler: CommandScheduler):
         scheduler.registerSubsystem(self)
         
-        self.main_motor = SparkFlex(config.effector_motor_id, SparkFlex.MotorType.kBrushless)
-        self.main_encoder = self.main_motor.getEncoder()
+        self.intake_motor = SparkFlex(config.effector_motor_id, SparkFlex.MotorType.kBrushless)
+        self.intake_encoder = self.intake_motor.getEncoder()
         self.wrist_motor = SparkFlex(config.wrist_motor_id, SparkFlex.MotorType.kBrushless)
-        self.wrist_encoder = PIDController(0, 0, 0.5) #TODO: Change to actual constants
+        self.wrist_encoder = self.wrist_motor.getEncoder()
+        self.pid_yippee = PIDController(0, 0, 0.5) #TODO: Change to actual constants
         self.position = 0 #can change, don't care. It's in radians?
         self.target = 0
+        
 
     def periodic(self):
-        #self.position = self.bob.getError()
-        encoder_value = self.main_encoder.getPosition()
-        self.main_motor.set(self.wrist_encoder.calculate(encoder_value, self.target))
-        
+        encoder_value = self.wrist_encoder.getPosition()
+        self.wrist_motor.set(self.pid_yippee.calculate(encoder_value, self.target))
     
     def spin(self, power):
-        self.main_motor.set(power)
+        self.intake_motor.set(power)
     
     def stopSpin(self):
-        self.main_motor.set(0)
+        self.intake_motor.set(0)
         
     def setTarget(self, target):
         self.target = target
