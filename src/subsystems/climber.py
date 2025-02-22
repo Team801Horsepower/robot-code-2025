808352f47db04c98c08ba59ebd889517b93467f1
TheNerdyFunnyComedian <hudsonjimenez@gmail.com>
2025-02-22 14:52:58 -0500

    Add loose framework for climber

--- src/subsystems/climber.py
+++ src/subsystems/climber.py
@@ -1,12 +1,14 @@
 from commands2 import CommandScheduler, Subsystem
-
+from rev import SparkMax, SparkFlex, SparkBaseConfig, SparkBase
+import config
 
 class Climber(Subsystem):
     def __init__(self, scheduler: CommandScheduler):
         scheduler.registerSubsystem(self)
+        self.climb_motor = SparkFlex(config.climb_motor_id, SparkFlex.MotorType.kBrushless)
 
     def periodic(self):
         pass
 
-    def run_climber(self, power: float):
-        pass
+    def run(self, power: float):
+        self.climb_motor.set(power)
