ea642e3dbe59578d9835d0367bf8578231a95169
Yilu <wangyilu02@gmail.com>
2025-02-24 19:31:11 -0500

    Turn Signal yay

--- src/subsystems/turn_signals.py
+++ src/subsystems/turn_signals.py
@@ -1,12 +1,18 @@
-from commands2 import CommandScheduler, Subsystem
+from commands2 import CommandScheduler, Subsystem, Time
 
 
 class TurnSignals(Subsystem):
     def __init__(self, scheduler: CommandScheduler):
         scheduler.registerSubsystem(self)
-
+        self.led = False # TODO: change to actual LED strip initialization
         self.signal_left = False
         self.signal_right = False
+        self.turnSignal = False
 
     def periodic(self):
+        if self.turnSignal:
+            if Time.time % 0.5 <= 0.25:
+                self.led = True
+            else:
+                self.led = False
         pass
