be9f2533b1601b294ca80cc9c7bff29b437a8a12
Omnikar <omnikar5@gmail.com>
2025-03-07 09:29:09 -0500

    (Untested) cleanup and slight fix

--- src/commands/target_reef.py
+++ src/commands/target_reef.py
@@ -22,8 +22,9 @@ class TargetReef(Command):
         self.tag_id = tag_ids[int(stalk_i / 2)]
         self.target_angle = 0 if config.is_red else pi
         self.target_angle += pi / 3 * int(stalk_i / 2)
-        self.x_pid = PIDController(15.0, 0, 0)
-        self.y_pid = PIDController(3.0, 0, 0)
+
+        self.approach_pid = PIDController(15.0, 0, 0)
+        self.strafe_pid = PIDController(3.0, 0, 0)
         self.theta_pid = PIDController(5.0, 5.0, 0.3)
 
         # TODO: after front cameras are pitched down, remeasure these values
@@ -37,7 +38,7 @@ class TargetReef(Command):
 
     def execute(self):
         new_p = SmartDashboard.getNumber("approach P", 15.0)
-        self.x_pid.setP(new_p)
+        self.approach_pid.setP(new_p)
 
         rot = self.drive.odometry.rotation().radians()
         target_rot = self.target_angle
@@ -46,6 +47,8 @@ class TargetReef(Command):
         while target_rot < rot - pi:
             target_rot += 2 * pi
         omega = self.theta_pid.calculate(rot, target_rot)
+        if abs(omega) > config.auto_turn_speed:
+            omega = omega * config.auto_turn_speed / abs(omega)
 
         def get_yaw(i: int) -> Optional[float]:
             targets = self.vision.results[i].getTargets()
@@ -58,37 +61,32 @@ class TargetReef(Command):
         right_yaw = get_yaw(2)
 
         if left_yaw is None or right_yaw is None:
-            drive_input = Transform2d(0, 0, omega)
+            drive_speed = Translation2d(0, 0)
         else:
-            # left_err = self.left_target - left_yaw
-            # right_err = self.right_target - right_yaw
-
             SmartDashboard.putNumber("left yaw", left_yaw)
             SmartDashboard.putNumber("right yaw", right_yaw)
             SmartDashboard.putNumber("left target", self.left_target)
             SmartDashboard.putNumber("right target", self.right_target)
 
-            left_power = self.y_pid.calculate(left_yaw, self.left_target)
-            right_power = self.y_pid.calculate(right_yaw, self.right_target)
+            left_power = self.strafe_pid.calculate(left_yaw, self.left_target)
+            right_power = self.strafe_pid.calculate(right_yaw, self.right_target)
             strafe_speed = (left_power + right_power) / 2
-            if abs(strafe_speed) > config.auto_drive_speed:
-                strafe_speed = (
-                    strafe_speed / abs(strafe_speed) * config.auto_drive_speed
-                )
-            SmartDashboard.putNumber("strafe power", strafe_speed)
+            SmartDashboard.putNumber("strafe speed", strafe_speed)
 
             diff = left_yaw - right_yaw
             target_diff = self.left_target - self.right_target
-            approach_speed = self.x_pid.calculate(diff, target_diff)
-            if abs(approach_speed) > config.auto_drive_speed:
-                approach_speed = (
-                    approach_speed / abs(approach_speed) * config.auto_drive_speed
-                )
+            approach_speed = self.approach_pid.calculate(diff, target_diff)
             SmartDashboard.putNumber("diff", units.radiansToDegrees(diff))
             SmartDashboard.putNumber("diff target", units.radiansToDegrees(target_diff))
-            SmartDashboard.putNumber("approach power", approach_speed)
+            SmartDashboard.putNumber("approach speed", approach_speed)
 
-            drive_input = Transform2d(approach_speed, strafe_speed, omega)
+            drive_speed = Translation2d(approach_speed, strafe_speed)
+
+        norm = drive_speed.norm()
+        if norm > config.auto_drive_speed:
+            drive_speed = drive_speed * config.auto_drive_speed / norm
+
+        drive_input = Transform2d(drive_speed, Rotation2d(omega))
 
         self.drive.drive(drive_input)
 
